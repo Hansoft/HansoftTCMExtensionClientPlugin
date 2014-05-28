2aa8debca581ab9e69452e364c4dac591b3a9120
samuelathansoft <samuel@hansoft.com>
2014-05-28 11:38:18 +0200

    Added the plugin installer executable

--- Installable/Uninstall.bat
+++ Installable/Uninstall.bat
@@ -1,1 +1,1 @@
-..\..\SAFeKit\PluginInstaller\HansoftClientPluginInstaller.exe -clocalhost:50257:"TestCaseMngmt":Jean:welcome -ucom.hansoft.tcmextension.tcmclientplugin
+..\PluginInstaller\HansoftClientPluginInstaller.exe -clocalhost:50257:"TestCaseMngmt":Jean:welcome -ucom.hansoft.tcmextension.tcmclientplugin
